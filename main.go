9a40529b9d52aeaef1dc07c2cba136e07dca13bb
adam <adamardiyantow@gmail.com>
2025-07-04 15:18:57 +0700

    Add template rendering for index and about pages with header and footer

--- main.go
+++ main.go
@@ -4,23 +4,35 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"path"
 )
 
+type M map[string]interface{}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	var filepath = path.Join("views", "index.html")
-	var tmpl, err = template.ParseFiles(filepath)
+	var data = M{"name": "Mr. Ard"}
+	var tmpl = template.Must(template.ParseFiles(
+		"views/index.html",
+		"views/_header.html",
+		"views/_footer.html",
+	))
+
+	var err = tmpl.ExecuteTemplate(w, "index", data)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
+}
 
-	var data = map[string]interface{}{
-		"title": "WEB-GO",
-		"name":  "Mr. ARD",
-	}
+func about(w http.ResponseWriter, r *http.Request) {
+	var data = M{"name": "Mr. Ard"}
+	var tmpl = template.Must(template.ParseFiles(
+		"views/about.html",
+		"views/_header.html",
+		"views/_footer.html",
+	))
 
-	err = tmpl.Execute(w, data)
+	var err = tmpl.ExecuteTemplate(w, "about", data)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -34,6 +46,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/index", index)
+	http.HandleFunc("/about", about)
 	http.HandleFunc("/hello", hello)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
 
